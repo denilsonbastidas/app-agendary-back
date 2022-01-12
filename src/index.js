f3b335e0d5d4b77e4246dc75ba46475ca2f197a7
denilsonbastidas <denilsonbastidas3@gmail.com>
2022-01-12 00:50:38 -0400

    push final

--- src/index.js
+++ src/index.js
@@ -2,13 +2,12 @@ const mongoose = require('mongoose');
 const app = require('./app');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect("mongodb://localhost/agendary")
     .then(() => {
         console.log("Connections Satifactory!!!");
 
         // creacion del servidor
-        app.listen(app.get('puert'),() =>{
+        app.listen(app.get('puert'), () => {
             console.log("server success", app.get('puert'));
         });
     });
-    // mongodb+srv://denilson:<password>@cluster0.veqqu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
